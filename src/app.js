75dc706f9b587f4a82be4874e278a4ba46630bcc
Khoa Le <ltkhoa2711@gmail.com>
2019-11-03 12:42:50 +1100

    draw the camera video feed onto a canvas

--- src/app.js
+++ src/app.js
@@ -1,10 +1,36 @@
 const video = document.querySelector('#camera-stream');
 
+// To extract the video frame data, we have to first draw it onto a canvas.
+// Only then can we extract pixel data from the canvas context, manipulate it,
+// and draw it onto another canvas.
+const hiddenCanvas = document.querySelector('#hidden-canvas');
+const outputCanvas = document.querySelector('#output-canvas');
+const hiddenContext = hiddenCanvas.getContext('2d');
+const outputContext = outputCanvas.getContext('2d');
+
 const constraints = {
   video: {
     width: 512,
     height: 512,
   },
+};
+
+const processFrame = () => {
+  const { videoWidth: width, videoHeight: height } = video;
+
+  if (width && height) {
+    hiddenCanvas.width = width;
+    hiddenCanvas.height = height;
+    outputCanvas.width = width;
+    outputCanvas.height = height;
+    hiddenContext.drawImage(video, 0, 0, width, height);
+
+    // get frame from hiddenContext
+    // apply filter
+    // draw outputContext
+  }
+
+  window.requestAnimationFrame(processFrame);
 };
 
 navigator.getUserMedia(
@@ -15,3 +41,8 @@ navigator.getUserMedia(
   },
   console.error
 );
+
+video.addEventListener('play', function () {
+  window.requestAnimationFrame(processFrame);
+  console.log('Live!');
+});
